73b788b9e18d879898b5fc7af4980991cd36c0a4
cs7eric <csq020611@gmail.com>
2025-04-17 19:20:01 +0800

    release v1.1 bugfix: 修复刷新后404问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 import App from "./App.tsx";
-import { Provider } from "./provider.tsx";
+import {Provider} from "./provider.tsx";
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Provider>
-        <App />
+        <App/>
       </Provider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 );
